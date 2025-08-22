186c7638e64bbbe37b731a465034bdef28f25994
Greg Kostin <gkostin@umich.edu>
2025-08-22 16:03:33 -0400

    passing all tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-// use std::fs::File;
-// use std::io::{self, BufRead, BufReader};
-
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug, Parser)]
 #[command(version)]
@@ -32,9 +31,48 @@ struct Args {
     number_nonblank_lines: bool,
 }
 
+fn open(filename: &str) -> Result<Box<dyn BufRead>, Box<dyn std::error::Error>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn output_lines(buf: Box<dyn BufRead>, number_all: bool, number_nonblank: bool) -> io::Result<()> {
+    let mut line_no_all: usize = 1;
+    let mut line_no_nonblank: usize = 1;
+
+    for line in buf.lines() {
+        let line = line?;
+        if number_all {
+            println!("{line_no_all:>6}\t{line}");
+            line_no_all += 1;
+        } else if number_nonblank {
+            if line.is_empty() {
+                println!();
+            } else {
+                println!("{line_no_nonblank:>6}\t{line}");
+                line_no_nonblank += 1;
+            }
+        } else {
+            println!("{line}");
+        }
+    }
+    Ok(())
+}
 fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
+    let number_all = args.number_lines;
+    let number_nonblank = args.number_nonblank_lines;
+
     for filename in args.files {
-        println!("{filename}");
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {filename}: {err}"),
+            Ok(buf) => {
+                if let Err(err) = output_lines(buf, number_all, number_nonblank) {
+                    eprintln!("Failed to read {filename}: {err}");
+                }
+            }
+        }
     }
     Ok(())
 }
